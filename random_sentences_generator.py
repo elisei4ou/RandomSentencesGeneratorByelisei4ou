0a465b83ffd18078b02f96b1c5d5ed18e581a9e3
elisei4ou <elisei.29@abv.bg>
2023-10-17 13:05:24 +0300

    some_changes

--- random_sentences_generator.py
+++ random_sentences_generator.py
@@ -1,10 +1,12 @@
 import random
+from colorama import Fore
 
 
 def get_random_word(words):
     return random.choice(words)
 
 
+when = ['Vchera', 'V momenta', 'Onziden', 'Ne znam koga', 'V 4.20']
 names = ['Seiko', 'Deska', 'Miro', 'Tisho', 'Krisa', 'Mariana', 'Achkata', 'Nikola', 'Ganev']
 places = ['Delisule', 'silo', 'grada', 'Sofia', 'Frencha', 'kenefa']
 verbs = ['hapva', 'igrae', 'cuka', 'gotvi', 'vijda']
@@ -14,22 +16,19 @@ details = ['na igrishteto', 'na mashinkite', 'v kafeneto', 'okolo barata', 'u ku
 
 
 while True:
-    do_you_want_next = input("Ako iskash random izrechenie napishi: pls brat, ako ne iskash napishi: mn si zle: ")
+    do_you_want_next = input(Fore.LIGHTGREEN_EX + "Ako iskash random izrechenie napishi: pls brat, ako ne iskash napishi: mn si zle: ")
     if do_you_want_next == "pls brat":
+        random_time = get_random_word(when)
         random_name = get_random_word(names)
         random_place = get_random_word(places)
         random_verb = get_random_word(verbs)
         random_noun = get_random_word(nouns)
         random_adverb = get_random_word(adverbs)
         random_detail = get_random_word(details)
-        print(f"{random_name} ot {random_place} {random_adverb} {random_verb} {random_noun} {random_detail}")
+        print(Fore.RED + f"{random_time} {random_name} ot {random_place} {random_adverb} {random_verb} {random_noun} {random_detail}")
     elif do_you_want_next == "mn si zle":
-        print("Em ako shtesh ve")
+        print(Fore.LIGHTBLUE_EX + "Em ako shtesh ve")
         break
     else:
-        print("bugna mashinata brat... bral")
+        print(Fore.LIGHTRED_EX + "bugna mashinata brat... bral")
         print("are opitai pak")
-
-
-
-
